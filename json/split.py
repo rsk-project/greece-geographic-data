dd8b40ab941aa7a17758e6c48381888e222d9628
George Theofilis <theofilis.g@gmail.com>
2015-09-29 12:09:44 +0300

    Generate sub entities

--- json/split.py
+++ json/split.py
@@ -1,6 +1,6 @@
 i = 0
-with open('county.json') as in_file:
+with open('../scripts/villages.json') as in_file:
 	for line in in_file.readlines():
 		i = i + 1
-		with open('county/%d.json' % i , 'w') as out_file:
+		with open('../json/villages/%d.json' % i , 'w') as out_file:
 			print >> out_file, line
